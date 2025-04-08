17892d967a2ed8d0a8f21bf7b09434d7d5eb5a34
Nikita Sokolov <nikita436@gmail.com>
2025-04-08 22:07:42 +0700

    Initialize webpack

--- ci-template/webpack.config.js
+++ ci-template/webpack.config.js
@@ -1,10 +1,13 @@
-
-const path = require('node:path');
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
   },
   module: {
     rules: [
@@ -25,7 +28,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -41,6 +44,11 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
   ],
+  devtool: 'source-map',
+
 };
